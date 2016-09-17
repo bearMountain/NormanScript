232dfb42980368e47933febedeea1bc1440431c7
Jeffrey Camealy <jeffrey@theworklog.com>
2016-09-17 13:04:41 -0700

    Initial Commit

---
+++ NormanScript/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NormanScript
+//
+//  Created by Jeffrey Camealy on 9/17/16.
+//  Copyright © 2016 BranchComputing. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
